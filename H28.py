704127034a0f12a99a5cab52c547ef5a9de4a2d0
mnpawan <mnpawan@gmail.com>
2021-02-22 07:37:23 -0500

    checkin
    check in

--- H28.py
+++ H28.py
@@ -1,7 +1,9 @@
 class Piglet:
+    """hi my friend i am trying to calculate the pg years"""
     years = 0
 
     def pig_years(self):
+        """hi my friend i am trying to calculate the pg years"""
         return self.years * 18
 
 
@@ -16,3 +18,6 @@ print(piggy.pig_years())
 piggy.years=5
 
 print(piggy.pig_years())
+
+
+
