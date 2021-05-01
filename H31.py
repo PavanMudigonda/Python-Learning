4bbcc2293a5294ccc91f55b6a70aeeaec7db22c8
mnpawan <mnpawan@gmail.com>
2021-04-30 22:13:41 -0400

    Merge remote-tracking branch 'origin/main'

--- H31.py
+++ H31.py
@@ -1,6 +1,6 @@
 class Person:
     def __init__(self, name):
-        self.name = ___
+        self.name = ''
     def greeting(self):
         # Should return "hi, my name is " followed by the name of the Person.
         return print("hi, my name is " + self.person)
