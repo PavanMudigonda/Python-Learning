b2ad758c596e6e7ef9a947baf7117475dede9e67
mnpawan <mnpawan@gmail.com>
2021-07-20 21:28:43 -0400

    Merge remote-tracking branch 'origin/main'

--- lists-tuple.py
+++ lists-tuple.py
@@ -3,7 +3,7 @@ for x in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]:
     print(x)
     print()
 
-# Lists for numbers
+# Lists for strings
 for animal in ['cat','dog','monkey','donkey','pig','frog','fish','hen','chicken','goat']:
     print(animal)
     print()
@@ -26,4 +26,4 @@ for months in months:
 
 cats = ['Tom', 'Snappy', 'Kitty', 'Jessie', 'Chester']
 
-print cats[2]
+print(cats[2])
